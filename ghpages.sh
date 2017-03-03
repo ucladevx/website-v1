65e7b2de8f75f6a65f3e9ca99f4c286970109110
Kevin Wang <wangkevin448@gmail.com>
2017-03-02 20:14:15 -0800

    first commit

---
+++ ghpages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./build.sh
+cp pages/index.html ../landingPage-ghpages
+cp lib/devx_landing_page.css ../landingPage-ghpages
+cp lib/devx_landing_page.js ../landingPage-ghpages
