eaebd3cb536a1dbd16ce3cf60824f12e3d84193b
Kevin Wang <wangkevin448@gmail.com>
2017-03-03 22:10:13 -0800

    sections

--- src/main.js
+++ src/main.js
@@ -13,15 +13,30 @@ class App extends React.Component {
       <Navbar />
       <Header />
       <Section>
-        Section 1
+        <div className="blurb">
+          <h1 className="title">About Us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet felis urna. Mauris id fringilla sapien. Nam eleifend malesuada tristique. Suspendisse maximus interdum posuere. In ipsum sapien, hendrerit non dapibus et, luctus eu sem. Suspendisse sed ipsum erat.</p>
+          <p>Aenean maximus purus ultricies libero fermentum, efficitur facilisis dolor pharetra. Praesent et magna ac nibh ultrices efficitur. Proin congue euismod lobortis. Donec eget egestas purus. Sed dignissim nunc arcu, ullamcorper viverra libero interdum convallis. Pellentesque id interdum lacus. Praesent tempus, enim vel finibus porttitor, sem massa scelerisque justo, id suscipit lorem mauris vel sem.</p>
+        </div>
+        <div><img src="/assets/about_us.png"/></div>
       </Section>
       <Section>
-        Section 2
+        <div><img src="/assets/initiatives.png"/></div>
+        <div className="blurb">
+          <h1 className="title">Initiatives</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet felis urna. Mauris id fringilla sapien. Nam eleifend malesuada tristique. Suspendisse maximus interdum posuere. In ipsum sapien, hendrerit non dapibus et, luctus eu sem.</p>
+          <p>Aenean maximus purus ultricies libero fermentum, efficitur facilisis dolor pharetra. Praesent et magna ac nibh ultrices efficitur. Proin congue euismod lobortis. Donec eget egestas purus. Sed dignissim nunc arcu, ullamcorper viverra libero interdum convallis. Praesent tempus, enim vel finibus porttitor, sem massa scelerisque justo, id suscipit lorem mauris vel sem.</p>
+        </div>
       </Section>
       <Section>
-        Section 3
+        <div className="blurb">
+          <h1 className="title">Join Us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet felis urna. Mauris id fringilla sapien. Nam eleifend malesuada tristique. Suspendisse maximus interdum posuere. In ipsum sapien, hendrerit non dapibus et, luctus eu sem. Suspendisse sed ipsum erat.</p>
+          <p>Aenean maximus purus ultricies libero fermentum, efficitur facilisis dolor pharetra. Praesent et magna ac nibh ultrices efficitur. Proin congue euismod lobortis. Donec eget egestas purus. Sed dignissim nunc arcu, ullamcorper viverra libero interdum convallis. Pellentesque id interdum lacus.</p>
+          <p>Praesent tempus, enim vel finibus porttitor, sem massa scelerisque justo, id suscipit lorem mauris vel sem.</p>
+        </div>
+        <div><img src="/assets/join_team.png"/></div>
       </Section>
-      Hello World!
     </div>;
   }
 }
