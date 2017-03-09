5e17959e30f92a227319261e53bbd62c3592bf58
Kevin Wang <wangkevin448@gmail.com>
2017-03-09 12:34:28 -0800

    footer links

--- src/footer/index.js
+++ src/footer/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import Config from 'config';
+
+import {Anchor} from 'misc';
+
 class Footer extends React.Component {
   render(){
     return <footer className="footer">
@@ -8,10 +12,10 @@ class Footer extends React.Component {
         <div className="info">
           <h2>Dev<span className="devx-footer-x">X</span></h2>
           <div className="icons">
-            <a href="#"><img src="/assets/facebook_icon.png"/></a>
-            <a href="#"><img src="/assets/github_icon.png"/></a>
-            <a href="#"><img src="/assets/mail_icon.png"/></a>
-            <a href="#"><img src="/assets/youtube_icon.png"/></a>
+            <Anchor ext href={Config.url.social.fb}><img src="/assets/facebook_icon.png"/></Anchor>
+            <Anchor ext href={Config.url.social.gh}><img src="/assets/github_icon.png"/></Anchor>
+            <Anchor ext href={Config.url.social.em}><img src="/assets/mail_icon.png"/></Anchor>
+            <Anchor ext href={Config.url.social.yt}><img src="/assets/youtube_icon.png"/></Anchor>
           </div>
           <small>Copyright &copy; 2017 UCLA DevX</small>
         </div>
