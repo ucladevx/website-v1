0e6598a020ccfe5d839b9b6deaf216e904b39c87
Kevin Wang <wangkevin448@gmail.com>
2017-03-09 12:13:01 -0800

    navbar weird rendering

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export default {
+  url: {
+    apply: '#',
+    mailingList: '#',
+    workRequest: '#',
+    social: {
+      fb: '#',
+      gh: '#',
+      em: '#',
+      yt: '#',
+    },
+  },
+}
