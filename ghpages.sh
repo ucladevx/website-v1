97a7e8e86d9615ee04c2c4f191bcc386c2493430
Kevin Wang <wangkevin448@gmail.com>
2017-03-10 13:20:41 -0800

    linear gradient

--- ghpages.sh
+++ ghpages.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ./build.sh
-cp pages/index.html ../landingPage-ghpages
-cp lib/devx_landing_page.css ../landingPage-ghpages
-cp lib/devx_landing_page.js ../landingPage-ghpages
+mkdir -p ../ghpages-website
+cp -r pages/* ../ghpages-website
+cp lib/devx_landing_page.css ../ghpages-website
+cp lib/devx_landing_page.js ../ghpages-website
