e0c734f8ed55b49a47715b213eea8c79ee0801dd
Ram Goli <ramsgoli@gmail.com>
2018-09-09 18:55:46 -0700

    feat(webpack-plugin): add process.env plugin

--- src/config.js
+++ src/config.js
@@ -1,56 +1,62 @@
-import spring2017 from 'data/spring2017';
-import fall2017 from 'data/fall2017';
-import winter2018 from 'data/winter2018';
+import spring2017 from "data/spring2017";
+import fall2017 from "data/fall2017";
+import winter2018 from "data/winter2018";
 
 export default {
   url: {
-    apply: 'https://recruiting.ucladevx.com',
-    applyPM: 'https://recruiting.ucladevx.com',
-    mailingList: 'http://eepurl.com/cFOmXH',
-    workRequest: 'https://goo.gl/forms/ZmiidAbizEhYDMnG3',
-    api: 'http://api.ucladevx.com',
+    apply: "https://recruiting.ucladevx.com",
+    applyPM: "https://recruiting.ucladevx.com",
+    mailingList: "http://eepurl.com/cFOmXH",
+    workRequest: "https://goo.gl/forms/ZmiidAbizEhYDMnG3",
+    api: "http://api.ucladevx.com",
     demoday: {
-      rsvp: 'http://google.com',
-      sponsor: 'http://google.com',
+      rsvp: "http://google.com",
+      sponsor: "http://google.com"
     },
     social: {
-      fb: 'https://www.facebook.com/groups/ucladevx/',
-      gh: 'https://github.com/ucladevx',
-      em: 'mailto:ucladevx@gmail.com',
-      yt: 'https://www.youtube.com/channel/UCf4uTKuKJe-Pg3zFRYpfNpA',
+      fb: "https://www.facebook.com/groups/ucladevx/",
+      gh: "https://github.com/ucladevx",
+      em: "mailto:ucladevx@gmail.com",
+      yt: "https://www.youtube.com/channel/UCf4uTKuKJe-Pg3zFRYpfNpA"
     },
     sponsor: {
-      brochure: '/assets/sponsorship_brochure.pdf',
-      stickermule: 'https://www.stickermule.com/',
-    },
+      brochure: "/assets/sponsorship_brochure.pdf",
+      stickermule: "https://www.stickermule.com/"
+    }
   },
   demoday: {
-    time: 1521082800000,
+    time: 1521082800000
   },
-  years: ['spring2017', 'fall2017', 'winter2018'],
+  years: ["spring2017", "fall2017", "winter2018"],
   yearnames: {
-    'spring2017': {
-      stylized: 'Spring 2017',
-      name: 'Apollo',
-      blurb: ''
+    spring2017: {
+      stylized: "Spring 2017",
+      name: "Apollo",
+      blurb: ""
     },
-    'fall2017': {
-      stylized: 'Fall 2017',
-      name: 'Cassini'
+    fall2017: {
+      stylized: "Fall 2017",
+      name: "Cassini"
     },
-    'winter2018': {
-      stylized: 'Winter 2018',
-      name: 'Dawn'
+    winter2018: {
+      stylized: "Winter 2018",
+      name: "Dawn"
     }
   },
   projects: {
-    'spring2017': spring2017.data.teams,
-    'fall2017': fall2017.data.teams,
-    'winter2018': winter2018.data.teams,
+    spring2017: spring2017.data.teams,
+    fall2017: fall2017.data.teams,
+    winter2018: winter2018.data.teams
   },
   team: {
-    'spring2017': spring2017.data.members,
-    'fall2017': fall2017.data.members,
-    'winter2018': winter2018.data.members,
+    spring2017: spring2017.data.members,
+    fall2017: fall2017.data.members,
+    winter2018: winter2018.data.members
   },
+  gaTracking: {
+    trackingId: "UA-125060783-1",
+    options: {
+      debug: !process.env.PRODUCTION
+    }
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const env = process.env;
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, "src"),
   entry: {
-    devx_landing_page: 'main.js',
+    devx_landing_page: "main.js"
   },
   output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "lib"),
+    filename: "[name].js"
   },
   module: {
     rules: [
@@ -17,31 +18,36 @@ module.exports = {
         test: /\.s?css$/,
         use: ExtractTextPlugin.extract({
           use: [
-            {loader: "css-loader", options: {minimize: true}},
-            {loader: "sass-loader"},
+            { loader: "css-loader", options: { minimize: true } },
+            { loader: "sass-loader" }
           ]
-        }),
+        })
       },
       {
         test: /\.js$/,
-        use: [{loader: "babel-loader"}],
-      },
-    ],
+        use: [{ loader: "babel-loader" }]
+      }
+    ]
   },
   resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
   plugins: [
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin("[name].css"),
+    new webpack.DefinePlugin({
+      "process.env": {
+        PRODUCTION: JSON.stringify(env.NODE_ENV === "production")
+      }
+    })
   ],
   watchOptions: {
     aggregateTimeout: 500,
     poll: 2000,
-    ignored: /node_modules/,
+    ignored: /node_modules/
   },
   devServer: {
-    contentBase: [path.join(__dirname, 'lib'), path.join(__dirname, 'pages')],
+    contentBase: [path.join(__dirname, "lib"), path.join(__dirname, "pages")],
     compress: true,
-    port: 8000,
-  },
+    port: 8000
+  }
 };
